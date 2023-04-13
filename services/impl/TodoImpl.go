415d89d4a94c1881a2d9359daf1911360546680a
Vinicius D. Alves <roronoadiogo@gmail.com>
2023-04-13 18:23:41 -0300

    chore: understand interfaces and adding in the code

--- services/impl/TodoImpl.go
+++ services/impl/TodoImpl.go
@@ -7,11 +7,30 @@ import (
 type TodoImpl struct {
 }
 
-func (t TodoImpl) ListAll() []model.Todo {
+func (t TodoImpl) AddTodo(model.Todo) error {
+	return nil
+}
+
+func (t TodoImpl) UpdateTodo(id int) (model.Todo, error) {
+	todoUpdate := model.Todo{
+		ID:          1,
+		Name:        "Teste todo",
+		Description: "some descrition",
+		Finish:      true,
+		Position:    2,
+	}
+	return todoUpdate, nil
+}
+
+func (t TodoImpl) DeleteTodo(id int) error {
+	return nil
+}
+
+func (t TodoImpl) ListAll() ([]model.Todo, error) {
 	todos := []model.Todo{
 		{ID: 1, Name: "Ver emails",
 			Description: "Fazer as paradas",
 			Finish:      false, Position: 0},
 	}
-	return todos
+	return todos, nil
 }
